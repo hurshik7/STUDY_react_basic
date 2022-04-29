980727ed68f192bec23c7bcb50460d05dc102954
hurshik7 <hurshik7@gmail.com>
2022-04-28 17:52:53 -0700

    implement index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App /> {/* component App.js의 결과를 여기다가 출력*/}
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
