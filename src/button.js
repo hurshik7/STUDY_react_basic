a17b2f807f2e5d911ab22c7ec5cd507d1bbd92e7
hurshik7 <hurshik7@gmail.com>
2022-04-29 15:55:30 -0700

    implement click event

--- src/button.js
+++ src/button.js
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 export default function ({value}) {
+    const [isClicked, setIsClicked] = useState(false);
+    let className = 'alphabet-button';
+    if (isClicked) {
+        className += ' guessed';
+    }
+    
+    function clickHandler() {
+        setIsClicked(true);
+    }
+    
     return (
-        <button className="alphabet-button">
+        <button 
+            className={className}
+            onClick={clickHandler}
+        >
             {value}
         </button>
     );
