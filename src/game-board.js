88bd2439b4b74058c3b2ffa4530783d7ecb6ae8a
hurshik7 <hurshik7@gmail.com>
2022-04-28 17:52:45 -0700

    implement game-board.js

---
+++ src/game-board.js
@@ -0,0 +1,12 @@
+import LetterGrid from "./letter-grid";
+
+export default function({secretWord}) {
+    return (
+        <div>
+            <LetterGrid 
+                secretWord={secretWord}
+                guessedLetters={['a', 'r']}
+            />
+        </div>
+    );
+}
